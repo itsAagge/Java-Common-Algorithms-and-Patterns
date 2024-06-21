343faeca7b7bb10608da635d24dd9f180fda0a2f
August Juel Aagaard <augustjaagaard@gmail.com>
2024-06-21 23:24:49 +0200

    Search done

--- src/Search/Binary/Main.java
+++ src/Search/Binary/Main.java
@@ -1,4 +1,45 @@
 package Search.Binary;
 
-public class Main {
+import Search.SetUp.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main { //Uses the Student class in the SetUp folder
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>(List.of(new Student("Andrew", 22), new Student("August", 24), new Student("Brian", 25), new Student("Charles", 19), new Student("Daniel", 26),new Student("Eric", 22), new Student("Mike", 21)));
+
+        String targetName = "Daniel";
+        Student foundStudent = binarySearch(students, targetName);
+        System.out.println(foundStudent);
+
+        targetName = "August";
+        foundStudent = binarySearch(students, targetName);
+        System.out.println(foundStudent);
+
+        String notInListName = "Henry";
+        Student notInListStudent = binarySearch(students, notInListName);
+        if (notInListStudent == null) {
+            System.out.println("Henry not found");
+        }
+    }
+
+    public static Student binarySearch(ArrayList<Student> list, String name) {
+        int start = 0;
+        int end = list.size();
+        while(start <= end) {
+            int middle = (end+start)/2;
+            Student candidate = list.get(middle);
+            if (candidate.getName().equals(name)) {
+                return candidate;
+            } else {
+                if (candidate.getName().compareTo(name) > 0) {
+                    end = middle - 1;
+                } else {
+                    start = middle + 1;
+                }
+            }
+        }
+        return null;
+    }
 }
--- src/Search/Linear/Main.java
+++ src/Search/Linear/Main.java
@@ -1,4 +1,35 @@
 package Search.Linear;
 
+import Search.SetUp.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) { //Uses the Student class in the SetUp folder
+        ArrayList<Student> students = new ArrayList<>(List.of(new Student("Andrew", 22), new Student("August", 24), new Student("Brian", 25), new Student("Charles", 19), new Student("Daniel", 26),new Student("Eric", 22), new Student("Mike", 21)));
+
+        String targetName = "Daniel";
+        Student foundStudent = linearSearch(students, targetName);
+        System.out.println(foundStudent);
+
+        targetName = "August";
+        foundStudent = linearSearch(students, targetName);
+        System.out.println(foundStudent);
+
+        String notInListName = "Henry";
+        Student notInListStudent = linearSearch(students, notInListName);
+        if (notInListStudent == null) {
+            System.out.println("Henry not found");
+        }
+    }
+
+    public static Student linearSearch(ArrayList<Student> list, String name) {
+        for (Student student : list) {
+            if (student.getName().equals(name)) {
+                return student;
+            }
+        }
+        return null;
+    }
 }
