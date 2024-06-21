ab4b78ce2c59d1c60449f9683ddc43b5d383c930
drewl <andrewahaz@outlook.dk>
2024-06-21 23:41:13 +0200

    quicksort og mergesort

--- src/DivideAndConquer/Mergesort/Main.java
+++ src/DivideAndConquer/Mergesort/Main.java
@@ -1,4 +1,55 @@
 package DivideAndConquer.Mergesort;
 
+import java.util.ArrayList;
+
 public class Main {
+
+
+    private static void mergesort(ArrayList<Integer> list, int l, int h) {
+        if (l < h) {
+            int m = (l + h) / 2;
+            mergesort(list, l, m);
+            mergesort(list, m + 1, h);
+            merge(list, l, m, h);
+        }
+    }
+
+    // kombiner er realiseret ved fletteskabelonen
+    private static void merge(ArrayList<Integer> list, int low, int middle, int high) {
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        ArrayList<Integer> list1 = new ArrayList<>( list.subList(low, middle + 1));
+        ArrayList<Integer> list2 = new ArrayList<>(list.subList(middle + 1, high + 1));
+
+        int i1 = 0;
+        int i2 = 0;
+
+        while (i1 < list1.size() && i2 < list2.size()) {
+            if (list1.get(i1) <= list2.get(i2)) {
+                temp.add(list1.get(i1));
+                i1++;
+            }
+            else {
+                temp.add(list2.get(i2));
+                i2++;
+            }
+        }
+
+        while (i1 < list1.size()) {
+            temp.add(list1.get(i1));
+            i1++;
+        }
+
+        while (i2 < list2.size()) {
+            temp.add(list2.get(i2));
+            i2++;
+        }
+
+
+        for (int i = 0; i < temp.size(); i++) {
+            list.set(low + i, temp.get(i));
+        }
+
+    }
+
+
 }
--- src/DivideAndConquer/Quicksort/Main.java
+++ src/DivideAndConquer/Quicksort/Main.java
@@ -1,4 +1,46 @@
 package DivideAndConquer.Quicksort;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public class QuickSort {
+        private void swap(ArrayList<Integer> list, int k, int l) {
+            Integer e = list.get(k);
+            list.set(k, list.get(l));
+            list.set(l, e);
+        }
+
+        private void quicksort(ArrayList<Integer> list, int low, int high) {
+            if (low < high) {
+                int p = partition(list, low, high);
+                quicksort(list, low, p - 1);
+                quicksort(list, p + 1, high);
+            }
+        }
+
+        private int partition(ArrayList<Integer> list, int low, int high) {
+            int e = list.get(low);
+            int i = low + 1;
+            int j = high;
+            while (i <= j) {
+                if (list.get(i) <= e) {
+                    i++;
+                } else if (list.get(j) > e) {
+                    j--;
+                } else {
+                    swap(list, i, j);
+                    i++;
+                    j--;
+                }
+            }
+            swap(list, low, j);
+            return j;
+        }
+
+        public void quicksort(ArrayList<Integer> list) {
+            quicksort(list, 0, list.size() - 1);
+        }
+    }
+
 }
