9ecc04e62fadfda4147909b556cefdbb9145ee96
DanielKongen <Danielkonge1@Gmail.com>
2024-06-21 22:30:35 +0200

    Selection Sort

--- src/Sorting/Selectionsort/Main.java
+++ src/Sorting/Selectionsort/Main.java
@@ -1,4 +1,27 @@
 package Sorting.Selectionsort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    public int[] list = new int[10];
+
+    private void selectionSort() {
+        for (int i = 0; i < list.length; i++) {
+            int minPos = i;
+            int j;
+            for (j = i + 1; j < list.length;
+                 j++) {
+                if(list[i] < list[minPos]) {
+                    minPos = j;
+                }
+            }
+
+            if (minPos != i) {
+                list[minPos] = list[i];
+                list[i] = minPos;
+            };
+        }
+    }
 }
