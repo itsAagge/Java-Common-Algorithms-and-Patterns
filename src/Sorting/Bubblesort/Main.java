74b6a66df79a9176af4d7ed65ccddbcb264c1bb4
drewl <andrewahaz@outlook.dk>
2024-06-21 23:18:33 +0200

    swap bubblesort

--- src/Sorting/Bubblesort/Main.java
+++ src/Sorting/Bubblesort/Main.java
@@ -4,13 +4,19 @@ import static java.util.Collections.swap;
 
 public class Main {
     private int[] list = new int[10];
+
+    private static void swap(int[] list, int i, int j) {
+        int temp = list[i];
+        list[i] = list[j];
+        list[j] = temp;
+    }
     private void bubblesort() {
         for (int i = list.length - 1; i >= 0; i--) {
             int j;
             for (j = 0; j <= i - 1; j++);
             {
                 if (list[j] > list[j + 1]) {
-                    swap(list, j, j + 1);
+                   swap(list, i, j);
                 }
             }
         }
